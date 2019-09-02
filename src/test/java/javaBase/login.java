7a3ac7a44545b434190a0c3e5522078b850d94c1
1070604284@qq.com <32705597+zhoumary@users.noreply.github.com>
2019-09-02 17:09:48 +0800

    login
    1. abstract login class
    2. refactor main

--- src/test/java/javaBase/login.java
+++ src/test/java/javaBase/login.java
@@ -1,5 +1,23 @@
 package javaBase;
 
-public class login {
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class login {
+    @org.jetbrains.annotations.NotNull
+    static WebDriver loginS1(String user, String key, String url) {
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+
+        WebElement loginUser = driver.findElement(By.xpath("//input[@id='xs_username-inner']"));
+        loginUser.sendKeys(user);
+        WebElement loginPassword = driver.findElement(By.xpath("//input[@id='xs_password-inner']"));
+        loginPassword.sendKeys(key);
+        WebElement login = driver.findElement(By.xpath("//button[@id='logon_button']"));
+        if (login != null) {
+            login.click();
+        }
+        return driver;
+    }
 }
